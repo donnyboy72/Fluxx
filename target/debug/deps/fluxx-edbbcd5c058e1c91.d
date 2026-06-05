03500831d8e266cc413340adbdca658d5219c2aa
Donavan Watley <76824069+donnyboy72@users.noreply.github.com>
2026-06-05 14:24:44 -0400

    Learned how to use cargo clippy to make code better

---
+++ target/debug/deps/fluxx-edbbcd5c058e1c91.d
@@ -0,0 +1,10 @@
+C:\Users\donav\SWE\my_rust\rust_projects\fluxx\target\debug\deps\fluxx-edbbcd5c058e1c91.d: src\main.rs src\editor.rs Cargo.toml
+
+C:\Users\donav\SWE\my_rust\rust_projects\fluxx\target\debug\deps\libfluxx-edbbcd5c058e1c91.rmeta: src\main.rs src\editor.rs Cargo.toml
+
+src\main.rs:
+src\editor.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=-W__CLIPPY_HACKERY__clippy::all__CLIPPY_HACKERY__-W__CLIPPY_HACKERY__clippy::pedantic__CLIPPY_HACKERY__
+# env-dep:CLIPPY_CONF_DIR
