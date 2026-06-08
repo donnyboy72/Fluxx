c6d2a2529e4f55a3691d642e06abfc469437b6cf
Donavan Watley <76824069+donnyboy72@users.noreply.github.com>
2026-06-08 13:33:15 -0400

    added a editor folder with terminal.rs file. This file took on a lot of functions from the editor.rs file, allowing for cleaner code inside editor.rs

--- target/debug/deps/fluxx-4886341cd70b6263.d
+++ target/debug/deps/fluxx-4886341cd70b6263.d
@@ -1,6 +1,7 @@
-C:\Users\donav\SWE\my_rust\rust_projects\fluxx\target\debug\deps\fluxx-4886341cd70b6263.d: src\main.rs src\editor.rs
+C:\Users\donav\SWE\my_rust\rust_projects\fluxx\target\debug\deps\fluxx-4886341cd70b6263.d: src\main.rs src\editor.rs src\editor\terminal.rs
 
-C:\Users\donav\SWE\my_rust\rust_projects\fluxx\target\debug\deps\libfluxx-4886341cd70b6263.rmeta: src\main.rs src\editor.rs
+C:\Users\donav\SWE\my_rust\rust_projects\fluxx\target\debug\deps\libfluxx-4886341cd70b6263.rmeta: src\main.rs src\editor.rs src\editor\terminal.rs
 
 src\main.rs:
 src\editor.rs:
+src\editor\terminal.rs:
--- target/debug/deps/fluxx.d
+++ target/debug/deps/fluxx.d
@@ -1,6 +1,7 @@
-C:\Users\donav\SWE\my_rust\rust_projects\fluxx\target\debug\deps\fluxx.d: src\main.rs src\editor.rs
+C:\Users\donav\SWE\my_rust\rust_projects\fluxx\target\debug\deps\fluxx.d: src\main.rs src\editor.rs src\editor\terminal.rs
 
-C:\Users\donav\SWE\my_rust\rust_projects\fluxx\target\debug\deps\fluxx.exe: src\main.rs src\editor.rs
+C:\Users\donav\SWE\my_rust\rust_projects\fluxx\target\debug\deps\fluxx.exe: src\main.rs src\editor.rs src\editor\terminal.rs
 
 src\main.rs:
 src\editor.rs:
+src\editor\terminal.rs:
