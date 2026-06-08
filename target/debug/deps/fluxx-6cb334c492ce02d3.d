2dabf567a45e43f317d4c0bfa7701ee545b1f8cc
Donavan Watley <76824069+donnyboy72@users.noreply.github.com>
2026-06-07 23:57:43 -0400

    updated code to have more functions

--- target/debug/deps/fluxx-6cb334c492ce02d3.d
+++
@@ -1,5 +0,0 @@
-C:\Users\donav\SWE\my_rust\rust_projects\fluxx\target\debug\deps\fluxx-6cb334c492ce02d3.d: src\main.rs
-
-C:\Users\donav\SWE\my_rust\rust_projects\fluxx\target\debug\deps\libfluxx-6cb334c492ce02d3.rmeta: src\main.rs
-
-src\main.rs:
--- target/debug/deps/fluxx-f04fd32dc6538954.d
+++
@@ -1,5 +0,0 @@
-C:\Users\donav\SWE\my_rust\rust_projects\fluxx\target\debug\deps\fluxx-f04fd32dc6538954.d: src\main.rs
-
-C:\Users\donav\SWE\my_rust\rust_projects\fluxx\target\debug\deps\libfluxx-f04fd32dc6538954.rmeta: src\main.rs
-
-src\main.rs:
