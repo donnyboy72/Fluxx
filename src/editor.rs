e3702a54466deadaae3fbdb027dcfd7141b5ffc4
Donavan Watley <76824069+donnyboy72@users.noreply.github.com>
2026-06-10 15:38:53 -0400

    moved line rendering into it's own rs file, view.rs. This will allow for better rendering later on while keeping code clean

--- src/editor.rs
+++ src/editor.rs
@@ -4,11 +4,10 @@ use crossterm::event::{
     KeyCode, KeyEvent, KeyEventKind, KeyModifiers, read,
 };
 use std::{io::Error, thread::current};
-mod terminal;
 use terminal::{Position, Size, Terminal};
-
-const NAME: &str = env!("CARGO_PKG_NAME");
-const VERSION: &str = env!("CARGO_PKG_VERSION");
+mod terminal;
+mod view;
+use view::View;
 
 #[derive(Copy, Clone, Default)]
 struct Location {
@@ -111,7 +110,7 @@ impl Editor {
             Terminal::clear_screen()?;
             Terminal::print("Goodbye.\r\n");
         } else {
-            Self::draw_rows()?;
+            View::render()?;
             Terminal::move_caret_to(Position {
                 col: self.location.x,
                 row: self.location.y,
@@ -119,44 +118,6 @@ impl Editor {
         }
         Terminal::show_caret()?;
         Terminal::execute()?;
-        Ok(())
-    }
-
-    fn draw_welcome_message() -> Result<(), Error> {
-        let mut welcome_message = format!("{NAME} editor -- version {VERSION}");
-        let width = Terminal::size()?.width;
-        let len = welcome_message.len();
-
-        #[allow(clippy::integer_division)]
-        let padding = (width.saturating_sub(len)) / 2;
-
-        let spaces = " ".repeat(padding.saturating_sub(1));
-
-        welcome_message = format!("!{spaces}{welcome_message}");
-        welcome_message.truncate(width);
-        Terminal::print(welcome_message)?;
-        Ok(())
-    }
-
-    fn draw_empty_rows() -> Result<(), Error> {
-        Terminal::print("~");
-        Ok(())
-    }
-
-    fn draw_rows() -> Result<(), Error> {
-        let Size { height, .. } = Terminal::size()?;
-        for current_row in 0..height {
-            Terminal::clear_line()?;
-            #[allow(clippy::integer_division)]
-            if current_row == height / 3 {
-                Self::draw_welcome_message()?;
-            } else {
-                Self::draw_empty_rows()?;
-            }
-            if current_row.saturating_add(1) < height {
-                Terminal::print("\r\n")?;
-            }
-        }
         Ok(())
     }
 }
--- target/debug/deps/fluxx-4886341cd70b6263.d
+++ target/debug/deps/fluxx-4886341cd70b6263.d
@@ -1,10 +1,11 @@
-C:\Users\donav\SWE\my_rust\rust_projects\fluxx\target\debug\deps\fluxx-4886341cd70b6263.d: src\main.rs src\editor.rs src\editor\terminal.rs
+C:\Users\donav\SWE\my_rust\rust_projects\fluxx\target\debug\deps\fluxx-4886341cd70b6263.d: src\main.rs src\editor.rs src\editor\terminal.rs src\editor\view.rs
 
-C:\Users\donav\SWE\my_rust\rust_projects\fluxx\target\debug\deps\libfluxx-4886341cd70b6263.rmeta: src\main.rs src\editor.rs src\editor\terminal.rs
+C:\Users\donav\SWE\my_rust\rust_projects\fluxx\target\debug\deps\libfluxx-4886341cd70b6263.rmeta: src\main.rs src\editor.rs src\editor\terminal.rs src\editor\view.rs
 
 src\main.rs:
 src\editor.rs:
 src\editor\terminal.rs:
+src\editor\view.rs:
 
 # env-dep:CARGO_PKG_NAME=fluxx
 # env-dep:CARGO_PKG_VERSION=0.1.0
