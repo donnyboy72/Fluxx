fc760f700ec9e13205ece5810c7ea125439b2d07
Donavan Watley <76824069+donnyboy72@users.noreply.github.com>
2026-06-10 16:30:01 -0400

    Updated buffer and view to allow for a basic line viewer

--- src/editor.rs
+++ src/editor.rs
@@ -3,7 +3,7 @@ use crossterm::event::{
     Event::{self, Key},
     KeyCode, KeyEvent, KeyEventKind, KeyModifiers, read,
 };
-use std::{io::Error, thread::current};
+use std::{env, io::Error};
 use terminal::{Position, Size, Terminal};
 mod terminal;
 mod view;
@@ -25,9 +25,17 @@ pub struct Editor {
 impl Editor {
     pub fn run(&mut self) {
         Terminal::initialize().unwrap();
+        self.handle_args();
         let result = self.repl();
         Terminal::ternimate().unwrap();
         result.unwrap();
+    }
+
+    fn handle_args(&mut self) {
+        let args: Vec<String> = env::args().collect();
+        if let Some(file_name) = args.get(1) {
+            self.view.load(file_name);
+        }
     }
 
     fn repl(&mut self) -> Result<(), Error> {
